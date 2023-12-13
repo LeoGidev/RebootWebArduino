e309d7bd132a82d7bac7ec3a04ace9148ef69e02
LeoGidev <leogimenez.dev@gmail.com>
2023-12-13 14:00:03 -0300

    se completa funcion Rebooteo

--- rebootweb/rebootweb.ino
+++ rebootweb/rebootweb.ino
@@ -10,7 +10,7 @@ int RA=0;
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };//mac
 byte gateway[] = { 192,168,1,1}; //puerta
-byte subnet[] = { 255, 255, 255, 0 };//mascara
+byte subnet[] = { 255, 255, 255, 0};//mascara
 IPAddress ip(192,168,1,200); // configura la ip
 EthernetServer server(80);  // crea un servidor en el puerto 80
 File webFile;               // la pagina de la sd
@@ -31,36 +31,24 @@ void setup()
    pinMode(4, OUTPUT);
   digitalWrite(4, LOW);
     
-           
-    
-    // inicializamos sd
-    
-   if (!SD.begin(4)) {
-       
+  // inicializamos sd
+    if (!SD.begin(4)) {
        return;    
     }
-    
-    
     if (!SD.exists("index.htm")) {
-     
-        return;  
+     return;  
     }
     
   
-  Serial.begin(9600); 
+Serial.begin(9600); 
 Ethernet.begin(mac, ip);  //  le da los valores que antes indicamos de ip y mac al ethernet shield  
 server.begin();           // se pone a ver si hay clientes
     
     //setamos el pin que maneja el relé en nuesto caso el pin 2
-   
-    pinMode(2,OUTPUT);
+pinMode(2,OUTPUT);
     // Atencion! no es posible usar pinMode(4, OUTPUT) porque no se puede usar el pin 4 con ethernet shield
     //ponemos el pin en estado bajo
-    digitalWrite(2,LOW);
-   
-    
-    
-    
+digitalWrite(2,LOW);
 }
 
 void loop() {
@@ -127,12 +115,7 @@ void loop() {
                             client.println("HTTP/1.1 200 OK");
                             client.println();}
                             }
-                    
-                    
 
-                  
-             
-       /////////////////////////////////////////////
                         if (webFile) {
                         while(webFile.available()) {
                         client.write(webFile.read()); 
@@ -169,3 +152,30 @@ void loop() {
     } 
    
 }
+
+// funcion que rebootea
+void Rebooteo(void)
+{
+  EthernetClient client = server.available();
+  ////////////////////////////////////////////////////////////////////////////////
+   if (StrContains(HTTP_req, "&password")) {
+        if (StrContains(HTTP_req, "&RebootA&")) {
+        
+        digitalWrite(2,HIGH);
+        ok=1;
+        Serial.print(F("ok:")); Serial.println(ok);
+        delay(10000);
+        digitalWrite(2,LOW);
+       
+}
+    else if (StrContains(HTTP_req, "&RebootA&")==0){
+    ok=0;
+    
+     Serial.println(F("todo en0 1"));
+    }
+   }else{
+    ok=0;
+    
+    Serial.println(F("todo en0 2"));}
+    
+    }
